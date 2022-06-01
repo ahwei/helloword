0532b98e1840152b7680edff3e06bab73f76ddd5
AhWeiHENGuEi <aa9440307@gmail.com>
2022-06-01 12:59:25 +0800

    webpack

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,8 +2,15 @@ const { merge } = require("webpack-merge");
 const config = require("./webpack.config.js");
 const webpack = require("webpack");
 const CopyPlugin = require("copy-webpack-plugin");
+
+const path = require("path");
 module.exports = merge(config, {
   mode: "production",
+  // output: { 如果build 要在local打開需要這段設定
+  //   path: path.resolve(__dirname, "dist"),
+  //   publicPath: "./",
+  //   filename: `build-[contenthash].js`,
+  // },
   plugins: [
     new webpack.DefinePlugin({
       "process.env": {
